18ed1ae616a0dc02ab1c08c1638a704201488c81
Emily Shepherd <emily@redcoat.dev>
2021-10-11 00:18:35 +0100

    Add shortcut to call aws-vault
    The most common thing I do is dropping into a shell with a given profile
    so I can run multiple sets of commands.

--- zsh/aws-vault.zsh
+++ zsh/aws-vault.zsh
@@ -3,3 +3,7 @@ export AWS_VAULT_BACKEND=pass
 export AWS_VAULT_PASS_PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR
 export AWS_VAULT_PASS_PREFIX=aws-vault
 export GPG_TTY=$(tty)
+
+av() {
+    exec aws-vault exec $1 -- zsh
+}
